841acd4051ecad2e0ab35db6ab8f6b56043b6b9d
sanchit raghuwanshi <sanchit.raghu113@gmail.com>
2022-06-24 11:04:25 +0530

    add a procfile and port setting

--- app.js
+++ app.js
@@ -136,8 +136,12 @@ app.post("/submit",(req,res)=>{
 })
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-app.listen(3000,function(){
+app.listen(port,function(){
     console.log("server is started successfully");
 });
